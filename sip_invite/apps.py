5e5ca6020db78ede446bcac300d214765bfcb99d
ministre <vlgamer@mail.ru>
2020-12-11 18:07:42 +0300

    Initial commit

---
+++ sip_invite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SipInviteConfig(AppConfig):
+    name = 'sip_invite'
---
+++ tcp_udp_check/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TcpUdpCheckConfig(AppConfig):
+    name = 'tcp_udp_check'
