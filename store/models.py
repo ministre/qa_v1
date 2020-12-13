513974cf965e400da5e5c041e342d28c7dae1dfe
ministre <vlgamer@mail.ru>
2020-12-14 02:02:48 +0300

    Initial commit

--- store/models.py
+++ store/models.py
@@ -7,8 +7,8 @@ class Item(models.Model):
     name = models.CharField(max_length=500)
     location = models.CharField(max_length=500, blank=True, null=True)
     comment = models.CharField(max_length=500, blank=True, null=True)
-    date_of_received = models.DateField(default=timezone.now)
-    date_of_returned = models.DateField(default=None, blank=True, null=True)
+    date_of_received = models.DateTimeField(default=timezone.now)
+    date_of_returned = models.DateTimeField(default=None, blank=True, null=True)
     received_by = models.ForeignKey(User, related_name='received_by_user', on_delete=models.CASCADE)
     returned_by = models.ForeignKey(User, related_name='returned_by_user', on_delete=models.CASCADE, blank=True,
                                     null=True)
