7b1d5a12d3f647a1a5c6ab2d9fe500413e834098
ministre <vlgamer@mail.ru>
2020-12-14 00:38:03 +0300

    Initial commit

--- store/models.py
+++ store/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from datetime import datetime
 
 
 class Item(models.Model):
     name = models.CharField(max_length=500)
     location = models.CharField(max_length=500)
     comment = models.CharField(max_length=500)
-    date_of_received = models.DateField(default=timezone.now)
-    date_of_returned = models.DateField(default=timezone.now, blank=True, null=True)
-    received_by = models.ForeignKey(User, related_name='received_by_user', on_delete=models.CASCADE)
+    date_of_received = models.DateField(default=datetime.now, blank=True)
+    date_of_returned = models.DateField(default=None, blank=True, null=True)
+    received_by = models.ForeignKey(User, related_name='received_by_user', on_delete=models.CASCADE, blank=True)
     returned_by = models.ForeignKey(User, related_name='returned_by_user', on_delete=models.CASCADE, blank=True,
                                     null=True)
