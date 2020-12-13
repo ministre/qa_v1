e99bd6ff4977418a56235c403528957b89080e0e
ministre <vlgamer@mail.ru>
2020-12-14 00:12:30 +0300

    Initial commit

--- store/urls.py
+++ store/urls.py
@@ -3,13 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
-    path('<int:page_id>/', views.item_list, name='item_list'),
-    path('all/', views.item_full_list, name='item_full_list'),
-    path('all/<int:page_id>/', views.item_full_list, name='item_full_list'),
-    path('create/', views.item_create, name='item_create'),
-    path('return/<int:item_id>/', views.item_return, name='item_return'),
-    path('item/<int:item_id>/', views.item_edit, name='item_edit'),
-    path('search/', views.item_search, name='item_search'),
-    path('search/<int:page_id>/', views.item_search, name='item_search'),
+    path('', views.ItemListView.as_view(), name='items'),
+    path('create/', views.ItemCreate.as_view(), name='item_create'),
+    path('update/<int:pk>/', views.ItemUpdate.as_view(), name='item_update'),
+    path('delete/<int:pk>/', views.ItemDelete.as_view(), name='item_delete'),
+    path('return/<int:pk>/', views.item_return, name='item_return'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,85 +1,69 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from store.models import Item
 from .forms import ItemForm
 from django.http import HttpResponseRedirect
 from datetime import datetime
-from django.core.paginator import Paginator
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse
+
+
+@method_decorator(login_required, name='dispatch')
+class ItemListView(ListView):
+    context_object_name = 'items'
+    queryset = Item.objects.all().order_by("-id")
+    template_name = 'store/items.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class ItemCreate(CreateView):
+    model = Item
+    form_class = ItemForm
+    template_name = 'store/create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['back_url'] = reverse('items')
+        return context
+
+    def get_success_url(self):
+        return reverse('items')
+
+
+@method_decorator(login_required, name='dispatch')
+class ItemUpdate(UpdateView):
+    model = Item
+    form_class = ItemForm
+    template_name = 'store/update.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['back_url'] = reverse('items')
+        return context
+
+    def get_success_url(self):
+        return reverse('items')
+
+
+@method_decorator(login_required, name='dispatch')
+class ItemDelete(DeleteView):
+    model = Item
+    template_name = 'store/delete.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['back_url'] = reverse('items')
+        return context
+
+    def get_success_url(self):
+        return reverse('items')
 
 
 @login_required
-def item_full_list(request, page_id=1):
-    items = Item.objects.all().order_by("-id")
-    items_count = items.count()
-    current_page_items = Paginator(items, 50)
-    return render(request, 'store/item_full_list.html', {'items': current_page_items.page(page_id),
-                                                         'items_count': items_count})
-
-
-@login_required
-def item_list(request, page_id=1):
-    items = Item.objects.filter(returned_by=None).order_by("-id")
-    items_count = items.count()
-    current_page_items = Paginator(items, 50)
-    return render(request, 'store/item_list.html', {'items': current_page_items.page(page_id),
-                                                    'items_count': items_count})
-
-
-@login_required
-def item_create(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            new_item = Item(name=request.POST['name'], location=request.POST['location'], comment=request.POST['comment'],
-                            received_by=request.user)
-            new_item.save()
-            return HttpResponseRedirect('/store/')
-    else:
-        form = ItemForm()
-        return render(request, 'store/item_create.html', {'form': form})
-
-
-@login_required
-def item_edit(request, item_id):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            item = Item.objects.get(id=item_id)
-            item.name = request.POST['name']
-            item.location = request.POST['location']
-            item.comment = request.POST['comment']
-            item.save()
-            return HttpResponseRedirect('/store/')
-    else:
-        item = Item.objects.get(id=item_id)
-        form = ItemForm(initial={'name': item.name, 'location': item.location, 'comment': item.comment})
-        return render(request, 'store/item_edit.html', {'form': form, 'item': item})
-
-
-@login_required
-def item_return(request, item_id):
-    item = Item.objects.get(id=item_id)
+def item_return(request, pk):
+    item = Item.objects.get(id=pk)
     item.location = ''
     item.returned_by = request.user
     item.date_of_returned = datetime.now()
     item.save()
-    return HttpResponseRedirect('/store/')
-
-
-@login_required
-def item_search(request, page_id=1):
-    if 'q' in request.GET:
-        if request.GET['q'] == '':
-            return HttpResponseRedirect('/store/')
-        q = request.GET['q']
-        items = Item.objects.filter(name__icontains=q)
-        items_count = items.count()
-        current_page_items = Paginator(items, 50)
-        return render(request, 'store/item_search.html', {'items': current_page_items.page(page_id),
-                                                          'items_count': items_count,
-                                                          'q': q})
-
-#        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'Отсутствует запрос!'
-    return render(request, 'store/debug.html', {'message': message})
+    return HttpResponseRedirect(reverse('items'))
