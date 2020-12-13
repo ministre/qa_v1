08149255ca5cbebb707ec4d17187c2530fbe2160
ministre <vlgamer@mail.ru>
2020-12-14 00:46:32 +0300

    Initial commit

--- store/models.py
+++ store/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 class Item(models.Model):
     name = models.CharField(max_length=500)
-    location = models.CharField(max_length=500)
-    comment = models.CharField(max_length=500)
+    location = models.CharField(max_length=500, blank=True, null=True)
+    comment = models.CharField(max_length=500, blank=True, null=True)
     date_of_received = models.DateField(default=timezone.now)
     date_of_returned = models.DateField(default=None, blank=True, null=True)
     received_by = models.ForeignKey(User, related_name='received_by_user', on_delete=models.CASCADE)
