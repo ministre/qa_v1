b2c9f84bbe05ec572e28c360fae2c37194edfbe9
ministre <vlgamer@mail.ru>
2020-12-14 17:42:47 +0300

    Initial commit

--- testplan/models.py
+++ testplan/models.py
@@ -6,7 +6,7 @@ class TestPlan(models.Model):
     name = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.name
+        return str(self.name) + ' (' + str(self.version) + ')'
 
 
 class Test(models.Model):
