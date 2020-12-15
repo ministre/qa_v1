df72954d7c93156ec121d2f65689d6bb8e7f8339
ministre <vlgamer@mail.ru>
2020-12-15 22:47:26 +0300

    Initial commit

--- device/models.py
+++ device/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class DeviceType(models.Model):
-    main_type = models.CharField(max_length=50)
-    sub_type = models.CharField(max_length=50, blank=True, null=True)
     name = models.CharField(max_length=300)
+    redmine_project = models.CharField(max_length=100, blank=True, null=True)
+    redmine_project_name = models.CharField(max_length=1000, blank=True, null=True)
+    redmine_parent = models.CharField(max_length=1000, blank=True, null=True)
+    created_by = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    updated_at = models.DateTimeField(default=timezone.now)
+    main_type = models.CharField(max_length=50, blank=True, null=True)
+    sub_type = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
