a5acff7128cca72fcaf57078e0f4dd9a926e7df8
ministre <vlgamer@mail.ru>
2020-12-17 01:49:09 +0300

    Initial commit

--- device/models.py
+++ device/models.py
@@ -7,7 +7,7 @@ class DeviceType(models.Model):
     name = models.CharField(max_length=300)
     redmine_project = models.CharField(max_length=100, blank=True, null=True)
     redmine_project_name = models.CharField(max_length=1000, blank=True, null=True)
-    redmine_desc = models.CharField(max_length=1000, blank=True, null=True)
+    redmine_project_desc = models.CharField(max_length=1000, blank=True, null=True)
     redmine_parent = models.CharField(max_length=1000, blank=True, null=True)
     created_by = models.ForeignKey(User, models.SET_NULL, related_name='device_type_c', blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
