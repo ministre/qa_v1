2f6e52479729acbf5be61c625806bbdc1bc7fcf9
ministre <vlgamer@mail.ru>
2020-12-20 20:45:46 +0300

    Initial commit

--- testplan/models.py
+++ testplan/models.py
@@ -20,8 +20,8 @@ class TestPlan(models.Model):
 class Test(models.Model):
     testplan = models.ForeignKey(TestPlan, on_delete=models.CASCADE)
     category = models.CharField(max_length=300)
-    url = models.CharField(max_length=300)
     name = models.CharField(max_length=300)
+    url = models.CharField(max_length=300, null=True, blank=True)
     procedure = models.TextField()
     expected = models.TextField()
 
