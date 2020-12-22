a8a53522087047471392c536b694a02cd4969bad
ministre <vlgamer@mail.ru>
2020-12-22 23:29:49 +0300

    Initial commit

--- protocol/models.py
+++ protocol/models.py
@@ -37,7 +37,7 @@ class Protocol(models.Model):
 class TestResult(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
     protocol = models.ForeignKey(Protocol, on_delete=models.CASCADE)
-    comment = models.TextField(default=None, blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
     created_by = models.ForeignKey(User, models.SET_NULL, related_name='result_c', blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_by = models.ForeignKey(User, models.SET_NULL, related_name='result_u', blank=True, null=True)
