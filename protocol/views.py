d13424c2e72c4f66452458aedb61f7faa338ba55
ministre <vlgamer@mail.ru>
2020-12-23 04:01:40 +0300

    Initial commit

--- protocol/views.py
+++ protocol/views.py
@@ -440,13 +440,14 @@ def get_numbers_of_results(results):
 @login_required
 def restore_configs(request, pk):
     #protocol = get_object_or_404(Protocol, id=pk)
-    protocol = Protocol.objects.all()
-    if protocol.device.type.id < 4:
-        results = TestResult.objects.filter(protocol=protocol)
-        for result in results:
-            if result.config:
-                cfg = TestResultConfig.objects.create(result=result, config=result.config)
-                cfg.save()
+    protocols = Protocol.objects.all()
+    for protocol in protocols:
+        if protocol.device.type.id < 4:
+            results = TestResult.objects.filter(protocol=protocol)
+            for result in results:
+                if result.config:
+                    cfg = TestResultConfig.objects.create(result=result, config=result.config)
+                    cfg.save()
     return HttpResponseRedirect(reverse('protocol_details', kwargs={'pk': protocol.id, 'tab_id': 2}))
 
 
