e05bc8a7cbd03c1cc3b29f7366235649288f6e9e
ministre <vlgamer@mail.ru>
2020-12-24 21:10:54 +0300

    Initial commit

--- testplan/models.py
+++ testplan/models.py
@@ -46,7 +46,7 @@ class Category(models.Model):
 class Test(models.Model):
     cat = models.ForeignKey(Category, related_name='cat_test', on_delete=models.CASCADE)
     name = models.CharField(max_length=300)
-    purpose = models.TextField(max_length=5000, null=True, blank=True)
+    purpose = models.CharField(max_length=1000, null=True, blank=True)
     procedure = models.TextField()
     expected = models.TextField()
     priority = models.IntegerField(default=0)
