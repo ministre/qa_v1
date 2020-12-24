8624751cb84d8dd5ac94d785b34df1a60d837d25
ministre <vlgamer@mail.ru>
2020-12-24 18:02:01 +0300

    Initial commit

--- testplan/models.py
+++ testplan/models.py
@@ -57,7 +57,7 @@ class Test(models.Model):
     #
     url = models.CharField(max_length=300, null=True, blank=True)
     testplan = models.ForeignKey(TestPlan, on_delete=models.CASCADE, null=True, blank=True)
-    category = models.CharField(max_length=300)
+    category = models.CharField(max_length=300, null=True, blank=True)
 
     def __str__(self):
         return self.name
