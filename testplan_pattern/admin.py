dc4601f0715e28f4923fdac23dc846930aab8ab2
ministre <vlgamer@mail.ru>
2021-01-02 00:34:09 +0300

    Initial commit

--- testplan_pattern/admin.py
+++ testplan_pattern/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TestplanPattern, CategoryPattern
+from .models import TestplanPattern, CategoryPattern, TestPattern
 
 admin.site.register(TestplanPattern)
 admin.site.register(CategoryPattern)
+admin.site.register(TestPattern)
