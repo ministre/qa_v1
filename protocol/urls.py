b29539117e4db635280b133b1fd61c820675e1c1
ministre <vlgamer@mail.ru>
2021-01-03 18:52:45 +0300

    Initial commit

--- protocol/urls.py
+++ protocol/urls.py
@@ -30,5 +30,4 @@ urlpatterns = [
     path('result/issue/delete/<int:pk>/', views.ResultIssueDelete.as_view(), name='result_issue_delete'),
 
     path('copy_results/', views.protocol_copy_results, name='protocol_copy_results'),
-    path('migrate/', views.migrate, name='protocol_migrate'),
 ]
