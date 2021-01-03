bd86e02a1007f94e5efacc82502239544f0ce75f
ministre <vlgamer@mail.ru>
2021-01-03 17:20:37 +0300

    Initial commit

--- protocol/admin.py
+++ protocol/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from protocol.models import Protocol, TestResult, TestResultConfig, TestResultIssue
-
-# Register your models here.
+from protocol.models import Protocol, TestResult, TestResultNote, TestResultConfig, TestResultImage, TestResultIssue
 
 admin.site.register(Protocol)
 admin.site.register(TestResult)
+admin.site.register(TestResultNote)
 admin.site.register(TestResultConfig)
+admin.site.register(TestResultImage)
 admin.site.register(TestResultIssue)
