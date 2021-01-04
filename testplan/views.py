4ad96d54a35157221b7fdffa8c64d036256e96a0
ministre <vlgamer@mail.ru>
2021-01-04 16:50:33 +0300

    Initial commit

--- testplan/views.py
+++ testplan/views.py
@@ -517,8 +517,8 @@ def migrate(request, pk):
     for category in categories:
         tests = Test.objects.filter(cat=category)
         for test in tests:
-            new_redmine_wiki = test.redmine_wiki.replace("http://lab.server.group/projects/basic_pmi_swiches/wiki/", "")
-            test.redmine_wiki =  new_redmine_wiki[0:-1]
+            new_redmine_wiki = test.redmine_wiki.replace("http://lab.server.group/projects/wireless_cpe/wiki/", "")
+            test.redmine_wiki = new_redmine_wiki[0:-1]
             test.save()
             i +=1
     return render(request, 'device/message.html', {'message': [True, i]})
