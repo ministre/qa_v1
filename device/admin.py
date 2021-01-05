376800d378e4e4f1891c694e51631a5d0c127044
ministre <vlgamer@mail.ru>
2021-01-05 22:06:23 +0300

    Initial commit

--- device/admin.py
+++ device/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from device.models import Vendor, DeviceType, Device
-
+from device.models import Vendor, DeviceType, Device, DeviceSample, DeviceSampleAccount
 
 admin.site.register(Vendor)
 admin.site.register(DeviceType)
 admin.site.register(Device)
+admin.site.register(DeviceSample)
+admin.site.register(DeviceSampleAccount)
--- device/models.py
+++ device/models.py
@@ -31,8 +31,6 @@ class DeviceType(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     updated_by = models.ForeignKey(User, models.SET_NULL, related_name='device_type_u', blank=True, null=True)
     updated_at = models.DateTimeField(default=timezone.now)
-    main_type = models.CharField(max_length=50, blank=True, null=True, default='predicate')
-    sub_type = models.CharField(max_length=50, blank=True, null=True, default='predicate')
 
     class Meta:
         ordering = ["name"]
@@ -78,3 +76,26 @@ class Device(models.Model):
         from protocol.models import Protocol
         count = Protocol.objects.filter(device=self).count()
         return count
+
+
+class DeviceSample(models.Model):
+    device = models.ForeignKey(Device, related_name='device_sample', on_delete=models.CASCADE)
+    sn = models.CharField(max_length=50)
+    desc = models.CharField(max_length=1000, blank=True, null=True)
+    created_by = models.ForeignKey(User, models.SET_NULL, related_name='sample_c', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey(User, models.SET_NULL, related_name='sample_u', blank=True, null=True)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.sn
+
+
+class DeviceSampleAccount(models.Model):
+    sample = models.ForeignKey(DeviceSample, related_name='sample_account', on_delete=models.CASCADE)
+    username = models.CharField(max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=50, blank=True, null=True)
+    created_by = models.ForeignKey(User, models.SET_NULL, related_name='sample_account_c', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey(User, models.SET_NULL, related_name='sample_account_u', blank=True, null=True)
+    updated_at = models.DateTimeField(default=timezone.now)
